051fe1f0d1597c0ed088dcd917a6291ae1b04dde
Oskar Kogut <kogutoskar123@gmail.com>
2026-01-18 16:37:05 +0100

    WIP physical devices

---
+++ FFVulkan/include/FFVulkan/VulkanPhysicalDevice.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <vector>
+
+#include "FFCore/Core/Types.h"
+#include "vulkan/vulkan_core.h"
+
+namespace FFVk
+{
+    struct PhysicalDevice
+    {
+        VkPhysicalDevice PhysicalDevice;
+        VkPhysicalDeviceProperties PhysicalDeviceProperties;
+        std::vector<VkQueueFamilyProperties> QueueFamilyProperties;
+        std::vector<VkBool32> SupportsPresent;
+        std::vector<VkSurfaceFormatKHR> SurfaceFormats;
+        VkSurfaceCapabilitiesKHR SurfaceCapabilities;
+        VkPhysicalDeviceMemoryProperties MemoryProperties;
+        std::vector<VkPresentModeKHR> PresentModes;
+    };
+
+    class VulkanPhysicalDevices
+    {
+    public:
+        VulkanPhysicalDevices() = default;
+        ~VulkanPhysicalDevices() = default;
+
+        // TODO: Why not in ctor
+        void Init(const VkInstance& vkInstance, const VkSurfaceKHR& surfaceKHR);
+        u32 SelectDevice(VkQueueFlags reqQueueType, bool supportsPresent);
+        const PhysicalDevice& SelectedDevice() const;
+
+    private:
+        std::vector<PhysicalDevice> _physicalDevices;
+        i32 _selectedDevice = -1;
+    };
+}
---
+++ FFVulkan/src/VulkanPhysicalDevice.cpp
@@ -0,0 +1,59 @@
+﻿#include "FFVulkan/VulkanPhysicalDevice.h"
+
+#include "FFVulkan/VulkanUtils.h"
+
+namespace FFVk
+{
+    void VulkanPhysicalDevices::Init(const VkInstance& vkInstance, const VkSurfaceKHR& surfaceKHR)
+    {
+        u32 numDevices{};
+        VK_CALL_AND_CHECK(vkEnumeratePhysicalDevices(vkInstance, &numDevices, nullptr),
+            "Couldn't enumerate physical devices")
+
+        LOG_MESSAGE("Num physical devices: %d", numDevices)
+
+        _physicalDevices.resize(numDevices);
+        std::vector<VkPhysicalDevice> physicalDevices;
+        physicalDevices.resize(numDevices);
+
+        VK_CALL_AND_CHECK(vkEnumeratePhysicalDevices(vkInstance, &numDevices, physicalDevices.data()),
+            "Couldn't enumerate physical devices")
+
+        for (u32 i = 0; i < numDevices; ++i)
+        {
+            const VkPhysicalDevice& device = physicalDevices[i];
+            _physicalDevices[i].PhysicalDevice = device;
+
+            vkGetPhysicalDeviceProperties(device, &_physicalDevices[i].PhysicalDeviceProperties);
+
+            const char* name = _physicalDevices[i].PhysicalDeviceProperties.deviceName;
+            const u32 apiVersion = _physicalDevices[i].PhysicalDeviceProperties.apiVersion;
+            
+            LOG_MESSAGE("Physical device: %hs\n "
+                        "API Version: %d. %d. %d. %d",
+                        name,
+                        VK_API_VERSION_VARIANT(apiVersion),
+                        VK_API_VERSION_MAJOR(apiVersion),
+                        VK_API_VERSION_MINOR(apiVersion),
+                        VK_API_VERSION_PATCH(apiVersion))
+
+            u32 numFamilies{};
+            vkGetPhysicalDeviceQueueFamilyProperties(device, &numFamilies, nullptr);
+
+            _physicalDevices[i].QueueFamilyProperties.resize(numFamilies);
+            _physicalDevices[i].SupportsPresent.resize(numFamilies);
+            
+            vkGetPhysicalDeviceQueueFamilyProperties(device, &numFamilies, _physicalDevices[i].QueueFamilyProperties.data());
+            
+
+        }
+    }
+
+    uint32_t VulkanPhysicalDevices::SelectDevice(VkQueueFlags reqQueueType, bool supportsPresent)
+    {
+    }
+
+    const PhysicalDevice& FFVk::VulkanPhysicalDevices::SelectedDevice() const
+    {
+    }
+}
